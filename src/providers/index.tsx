6367ab4c154f814beefc8a07aead01af63bae12f
JCFcodex <faustino.johncarl2003@gmail.com>
2026-02-27 10:45:15 +0800

    `feat: Integrate toast notifications`

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,7 +1,12 @@
 "use client";
 
 import { QueryProvider } from "@/providers/query-provider";
+import { ToastProvider } from "@/components/ui/toast";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <QueryProvider>{children}</QueryProvider>;
+  return (
+    <QueryProvider>
+      <ToastProvider>{children}</ToastProvider>
+    </QueryProvider>
+  );
 }
