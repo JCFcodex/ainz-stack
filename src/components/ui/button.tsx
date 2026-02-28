4bad952c85cad2627fba64a4bedf0bbf66f64308
JCFcodex <faustino.johncarl2003@gmail.com>
2026-02-28 14:27:24 +0800

    `tweak: refine global typography and button dimensions`

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -19,10 +19,10 @@ const buttonVariants = cva(
         link: "text-foreground underline-offset-4 hover:underline",
       },
       size: {
-        default: "h-9 px-4 py-2",
-        sm: "h-8 rounded-md px-3 text-xs",
-        lg: "h-10 rounded-lg px-6",
-        icon: "size-9",
+        default: "h-8 px-3 py-1.5",
+        sm: "h-7 rounded-md px-2.5 text-xs",
+        lg: "h-9 rounded-lg px-5",
+        icon: "size-8",
       },
     },
     defaultVariants: {
